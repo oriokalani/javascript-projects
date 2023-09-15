5a3c8b17609a9679a78ab9e989036e82c79e96a5
oriokalani <orio.kalani@gmail.com>
2023-09-14 21:36:30 -0500

    studio progress

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -8,7 +8,7 @@ let crewMassKg = astronautCount * averageAstronautMassKg;
 let fuelMassKg = 760000;
 let shuttleMassKg = 74842.31;
 let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg;
-let maximumMassLimit = 850000;
+let maximumMassLimit = 85000;
 let fuelTempCelsius = -225;
 let minimumFuelTemp = -300;
 let maximumFuelTemp = -150;
@@ -16,36 +16,7 @@ let fuelLevel = 100;
 let weatherStatus = 'clear';
 let preparedForLiftOff = true;
 
-// add logic below to verify total number of astronauts for shuttle launch does not exceed 7
 
-// add logic below to verify all astronauts are ready
-
-// add logic below to verify the total mass does not exceed the maximum limit of 850000
-
-// add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
-
-// add logic below to verify the fuel level is at 100%
-
-// add logic below to verify the weather status is clear
-
-// Verify shuttle launch can proceed based on above conditions
-if (astronautCount <= 7) {
-    if (astronautStatus === 'ready') {
-        if (totalMassKg < maximumMassLimit) {
-            if (fuelTempCelsius > minimumFuelTemp || fuelTempCelsius < maximumFuelTemp) {
-                if (fuelLevel === 100) {
-                    if (weatherStatus === 'clear') {
-                        preparedForLiftOff;
-                    }
-                }
-            }
-        }
-    }
-} else {
-    !preparedForLiftOff;
-}
-
-// CONSOLE OUTPUT
 let messageLaunchSequence = 'All systems are a go! Initiating space shuttle launch sequence.';
 let divider = '---------------------------------------------------------------';
 let messageDate = `Date: ${date}`;
@@ -57,6 +28,52 @@ let messageShuttleMass = `Shuttle Mass: ${shuttleMassKg} kg`;
 let messageTotalMass = `Total Mass: ${totalMassKg} kg`;
 let messageFuelTemperature = `Fuel Temperature: ${fuelTempCelsius} C`;
 let messageWeatherStatus = `Weather Status: ${weatherStatus}`;
+// add logic below to verify total number of astronauts for shuttle launch does not exceed 7
+
+// add logic below to verify all astronauts are ready
+
+// add logic below to verify the total mass does not exceed the maximum limit of 850000
+
+// add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
+
+// add logic below to verify the fuel level is at 100%
+
+// add logic below to verify the weather status is clear
+if (astronautCount <= 7) {
+    astronautStatus = 'ready';
+} else {
+    astronautStatus = 'not ready';
+}
+if (totalMassKg < maximumMassLimit) {
+    astronautCount = 'ready';
+} else {
+    astronautStatus = 'not ready';
+}
+if (fuelTempCelsius > minimumFuelTemp || fuelTempCelsius < maximumFuelTemp) {
+    astronautCount = 'ready';
+} else {
+    astronautStatus = 'not ready';
+}
+if (fuelLevel = )
+// Verify shuttle launch can proceed based on above conditions
+// if (astronautCount <= 7) {
+//     if (astronautStatus === 'ready') {
+//         if (totalMassKg < maximumMassLimit) {
+//             if (fuelTempCelsius > minimumFuelTemp || fuelTempCelsius < maximumFuelTemp) {
+//                 if (fuelLevel === 100) {
+//                     if (weatherStatus === 'clear') {
+//                         preparedForLiftOff;
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// } else {
+//     !preparedForLiftOff;
+//}
+
+
+// CONSOLE OUTPUT
 
 console.log(messageLaunchSequence);
 console.log(divider);
