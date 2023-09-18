6e23f0b8c92bf97767da372293277983feb112dd
oriokalani <orio.kalani@gmail.com>
2023-09-18 16:12:39 -0500

    exercises complete

--- arrays/exercises/part-five-arrays.js
+++ arrays/exercises/part-five-arrays.js
@@ -2,10 +2,22 @@ let str = 'In space, no one can hear you code.';
 let arr = ['B', 'n', 'n', 5];
 
 //1) Use the split method on the string to identify the purpose of the parameter inside the ().
-
+console.log('--- Part 01 ---');
+console.log(str);
+console.log();
+console.log(str.split(' '));
+console.log(str.split(''));
 //2) Use the join method on the array to identify the purpose of the parameter inside the ().
-
+console.log('--- Part 02 ---');
+console.log(arr);
+console.log();
+console.log(arr.join());
+console.log(arr.join('a'));
+console.log(arr.join(' '));
+console.log(arr.join(''));
 //3) Do split or join change the original string/array?
+console.log('--- Part 03 ---');
+
 
 //4) We can take a comma-separated string and convert it into a modifiable array. Try it! Alphabetize the cargoHold string, and then combine the contents into a new string.
 let cargoHold = "water,space suits,food,plasma sword,batteries";
