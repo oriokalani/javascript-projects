060ce78b1fcf66a0e92462ffb4974cf5709a169f
oriokalani <orio.kalani@gmail.com>
2023-10-09 19:46:46 -0500

    updated exercise

--- functions/exercises/functions-exercise.js
+++ functions/exercises/functions-exercise.js
@@ -31,4 +31,46 @@ function makeRectangle(width, height) {
     }
     return rectangle.slice(0, -1);
 }
-console.log(makeRectangle(5));
+console.log(makeRectangle(5, 3));
+// BREAK
+console.log();
+
+//TODO: makeDownwardStairs(height) function
+// Write a function makeDownwardStairs(height) that prints the staircase pattern shown below, with the given height. Use your makeLine function to do this.
+function makeDownwardStairs(height) {
+    let stairs = '';
+    for (let i = 0; i < height; i++) {
+        stairs += makeLine(i + 1) + '\n';
+    }
+    return stairs.slice(0, -1);
+}
+console.log(makeDownwardStairs(5));
+// BREAK
+console.log();
+
+//TODO: makeSpaceLine(numSpaces, numChars) function
+// Write a function makeSpaceLine(numSpaces, numChars) that returns a line with exactly the specified number of spaces, followed by the specified number of hashes, followed again by numSpaces more spaces.
+function makeSpaceLine(numSpaces, numChars) {
+    let spaces = '';
+    let chars = '';
+    let spaceLine = '';
+
+    for (let i = 0; i < numSpaces; i ++) {
+        spaces += '_';
+    }
+    for (let i = 0; i < numChars; i ++) {
+        chars += '#';
+    }
+    spaceLine = `${spaces}${chars}${spaces}`;
+    return spaceLine;
+}
+console.log(makeSpaceLine(3, 5));
+// BREAK
+console.log();
+
+//TODO: makeIsoscelesTriangle(height) function
+// Write a function makeIsoscelesTriangle(height) that returns a triangle of the given height.
+function makeIsoscelesTriangle(height) {
+    
+}
+console.log(makeIsoscelesTriangle(5));
