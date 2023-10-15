022f571a0eaf47baba84302fc6568b86a3d891b0
oriokalani <orio.kalani@gmail.com>
2023-10-15 12:07:52 -0500

    updated

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -6,7 +6,10 @@ let launchcode = {
     'Web Development',
     'Data Analysis',
     'Liftoff'
-  ]
+  ],
+  launchcodeOutput: () => {
+    //method called launchOutput(). This method will return a string
+  }
 };
 
 module.exports = launchcode;
